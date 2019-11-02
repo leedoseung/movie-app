4237dd75af2839931149c1d7fd4eee61a7a6f09c
LeeDooSeung <leedoseu@naver.com>
2019-11-02 23:56:19 +0900

    movie-app complate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Axios from 'axios';
 import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
   state = {
@@ -46,6 +47,7 @@ class App extends React.Component {
                     id={movie.id}
                     year={movie.year}
                     title={movie.title}
+                    genres={movie.genres}
                     summary={movie.summary}
                     poster={movie.medium_cover_image}/>
                 )
